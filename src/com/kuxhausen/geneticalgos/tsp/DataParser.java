b16a1b7490bf9ef36ad408f0c9811770a83b9e6d
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-05-06 23:47:01 -0500

    Implemented parser and moved test data location

--- src/com/kuxhausen/geneticalgos/tsp/DataParser.java
+++ src/com/kuxhausen/geneticalgos/tsp/DataParser.java
@@ -1,8 +1,40 @@
 package com.kuxhausen.geneticalgos.tsp;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class DataParser {
-	public static CityList parse(String filename){
-		//TODO
-		return new CityList();
+	public static CityList parse(String filename) throws FileNotFoundException{
+		Scanner in = new Scanner(new FileReader(filename));
+		
+		in.nextLine();
+		in.nextLine();
+		in.nextLine();
+		in.nextLine();
+		in.next();
+		in.next();
+		int numCities = in.nextInt();
+		in.nextLine();
+		in.nextLine();
+		in.nextLine();
+		
+		double[] x = new double[numCities];
+		double[] y = new double[numCities];
+		
+		for(int i = 0; i<numCities; i++){
+			in.next();
+			x[i] = in.nextDouble();
+			y[i] = in.nextDouble();
+		}
+		
+		in.close();
+		
+		
+		CityList cl = new CityList();
+		cl.numCities = numCities;
+		cl.cityX = x;
+		cl.cityY = y;
+		return cl;
 	}
 }
--- src/com/kuxhausen/geneticalgos/tsp/Runner.java
+++ src/com/kuxhausen/geneticalgos/tsp/Runner.java
@@ -1,13 +1,36 @@
 package com.kuxhausen.geneticalgos.tsp;
 
+import java.io.FileNotFoundException;
+
 public class Runner {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		if(args.length<1 || args[0] == null){
+			System.out.println("Error: Missing Filename");
+			return;
+		}
+		
+		String filename = "data/"+args[0];
+		System.out.println("Loading "+filename);
+		
+		try {
+			CityList cList = DataParser.parse(filename);
+		
+			
+			for(int i = 0 ; i< cList.numCities; i++){
+				System.out.println(i+" "+cList.cityX[i]+" "+cList.cityY[i]);
+			}
+		
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
